50f55874d955145c58a588186ea33bafc7ac9f40
zeuscristian-ai <cristianyaovia@gmail.com>
2025-05-01 14:57:00 +0100

    Update Details.jsx

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { TacheForm } from "../components/TacheForm";
 
 const Details = () => {
-  return <div>Details</div>;
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl sm:text-3xl font-bold">Détails de la Tâche</h1>
+      </div>
+      <div className="mt-10 max-w-xl">
+          <TacheForm  />
+      </div>
+    </>
+  );
 };
 
 export default Details;
